4080d0caf76da0b6be6a8c2cfed37e593b2d47b1
XiuJie Yang <xiujie.yang@uftwf.org>
2023-06-30 09:54:17 -0400

    update text

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/openshift-for-developers/hello
 
-go 1.16
+go 1.19
--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello World"
 	}
 
 	fmt.Fprintln(w, response)
@@ -28,7 +28,7 @@ func main() {
 	http.HandleFunc("/", helloHandler)
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "8080"
+		port = "8180"
 	}
 	go listenAndServe(port)
 
